25d64dc0ff0969ff72afb61fe3ea12c521328a3a
walterdybek <9406941@alumnos.ufv.es>
2025-04-14 21:22:36 +0200

    cotinuando base de la pagina

--- Red/Red/urls.py
+++ Red/Red/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Welcome to the Red project!')
+
+def room(request, room_name):
+    return HttpResponse(f'Room: {room_name}')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home),
 ]
