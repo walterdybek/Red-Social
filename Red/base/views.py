a4214d42092725a38068ee5bc77dd17f22803c93
walterdybek <9406941@alumnos.ufv.es>
2025-04-15 16:18:20 +0200

    .

--- Red/base/views.py
+++ Red/base/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+rooms = [   # List of rooms to be displayed in the template 
+    {'id':1,'name': 'Room 1', 'description': 'Description of Room 1'},
+    {'id':2,'name': 'Room 2', 'description': 'Description of Room 2'},
+    {'id':3,'name': 'Room 3', 'description': 'Description of Room 3'},
+]
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'resultado': 2 + 2})
+    context = {'rooms': rooms}  # Context dictionary to pass data to the template
+    return render(request, 'home.html', context)  # Pass the rooms list to the template
 
 def room(request):
     return render(request, 'room.html')
