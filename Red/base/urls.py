e5487db4329b47a57123b3a3488a7c402abfe428
walterdybek <9406941@alumnos.ufv.es>
2025-04-16 02:15:55 +0200

    base pricipal de foro

--- Red/base/urls.py
+++ Red/base/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('prueba/', views.prueba, name='prueba'),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>', views.updateRoom, name='update-room')
 ]
--- Red/base/views.py
+++ Red/base/views.py
@@ -1,19 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 
-rooms = [   # List of rooms to be displayed in the template 
-    {'id':1,'name': 'Room 1', 'description': 'Description of Room 1'},
-    {'id':2,'name': 'Room 2', 'description': 'Description of Room 2'},
-    {'id':3,'name': 'Room 3', 'description': 'Description of Room 3'},
-]
+
 
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()  # Fetch all Room objects from the database
     context = {'rooms': rooms}  # Context dictionary to pass data to the template
     return render(request, 'base/home.html', context)  # Pass the rooms list to the template
 
-def room(request):
+def room(request,pk):
+    room = Room.objects.get(id=pk)  # Fetch a specific Room object by its primary key (pk)
     return render(request, 'room.html')
 
 def prueba(request):
     return render(request, 'test.html')
+
+def createRoom(request):
+    form = RoomForm
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}  # Context dictionary to pass data to the template
+    return render(request, 'base/room_form.html',context)
+
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}  # Context dictionary to pass data to the template
+    return render(request, 'base/room_form.html',context)
+
