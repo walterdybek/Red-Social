8bb1185e5a1706280568a6276aa7acec38e40442
walterdybek <9406941@alumnos.ufv.es>
2025-04-19 18:13:46 +0200

    modificando el login

--- Red/base/models.py
+++ Red/base/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -42,3 +44,22 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:50]
+#profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthdate = models.DateField(null=True, blank=True)
+    ufv_email = models.EmailField(unique=True)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
--- Red/base/urls.py
+++ Red/base/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [ 
@@ -15,5 +16,6 @@ urlpatterns = [
     path('delete-message/<str:pk>', views.deleteMessage, name='delete-message'),
     path('update-user/', views.updateUser, name='update-user'),
     path('topics/', views.topicsPage, name='topics'),
+    path('activity/', views.activityPage, name='activity'),
 
 ]
